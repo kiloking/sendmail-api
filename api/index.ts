36b8738cc68bd89d60ab975f75c49260662156ff
wood.wu <kilokingw@gmail.com>
2024-09-20 16:00:07 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 3000;
@@ -11,52 +11,53 @@ app.use(bodyParser.json());
 
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
-  host: 'smtp.mailersend.net',
+  host: "smtp.elasticemail.com",
   port: 587, // Outlook SMTP port (587 for TLS)
   tls: {
-      ciphers: 'SSLv3',
+    ciphers: "SSLv3",
   },
   auth: {
-    user: process.env.NEXT_PUBLIC_EMAIL_USER, 
-    pass: process.env.NEXT_PUBLIC_EMAIL_PASS 
-},
+    user: "support@wuwood.com",
+    pass: "F602A51F55A03CB6B79F1DF79C7B7B7400E7",
+  },
 });
 app.get("/", (req, res) => res.send("Express on Vercel"));
 // 测试 API 的 GET 端点
-app.get('/test', (req, res) => {
-    res.status(200).send({ message: 'API is working at:' });
+app.get("/test", (req, res) => {
+  res.status(200).send({ message: "API is working at:" });
 });
 
 // 发送邮件的 API 端点
-app.post('/send-email', (req, res) => {
-    const { to, subject, text } = req.body;
-    // 检查 'to' 字段是否存在并且是字符串
-    if (!to || typeof to !== 'string') {
-        return res.status(400).send({ message: 'Invalid "to" field. It should be a string.' });
+app.post("/send-email", (req, res) => {
+  const { to, subject, text } = req.body;
+  // 检查 'to' 字段是否存在并且是字符串
+  if (!to || typeof to !== "string") {
+    return res
+      .status(400)
+      .send({ message: 'Invalid "to" field. It should be a string.' });
+  }
+  const toArray = to.split(",").map((email) => email.trim());
+  console.log('Converted "to" field:', toArray); //
+
+  const mailOptions = {
+    from: process.env.NEXT_PUBLIC_EMAIL_USER, // 发送邮件地址
+    to: toArray, //
+    subject: subject, // 邮件主题
+    text: text, // 邮件内容
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send({ message: "Error sending email", error });
+    } else {
+      return res.status(200).send({ message: "Email sent", info });
     }
-    const toArray = to.split(',').map(email => email.trim());
-    console.log('Converted "to" field:', toArray); //
-
-    const mailOptions = {
-        from: process.env.NEXT_PUBLIC_EMAIL_USER, // 发送邮件地址
-        to: toArray, // 
-        subject: subject, // 邮件主题
-        text: text // 邮件内容
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.status(500).send({ message: 'Error sending email', error });
-        } else {
-            return res.status(200).send({ message: 'Email sent', info });
-        }
-    });
+  });
 });
 
 // 启动服务器
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
 
 module.exports = app;
